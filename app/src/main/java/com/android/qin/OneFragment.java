1a7ac89735618657a597a12bc5e633184aeab974
wyntonchin <goodcar@163.com>
2018-10-18 16:39:36 +0800

    上传包名启动应用

--- app/src/main/java/com/android/qin/OneFragment.java
+++ app/src/main/java/com/android/qin/OneFragment.java
@@ -3,14 +3,17 @@ package com.android.qin;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.android.qin.one.ApkInstallPermissionActivity;
 import com.android.qin.one.RequestPermissionActivity;
+import com.android.qin.util.ToastUtil;
 
 
 /**
@@ -54,7 +57,19 @@ public class OneFragment extends Fragment {
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
                 startActivity(intent);
             });
+
+            mContentView.findViewById(R.id.test5).setOnClickListener(v -> {
+                        PackageManager packageManager = getActivity().getPackageManager();
+                        Intent intent =packageManager.getLaunchIntentForPackage("com.hismart.easylink");
+                        if(intent==null){
+                            ToastUtil.showShortToast("未安装");
+                        }else {
+                            startActivity(intent);
+                        }
+
+            });
         }
+
 
         // 缓存View判断是否含有parent, 如果有需要从parent删除, 否则发生已有parent的错误.
         ViewGroup parent = (ViewGroup) mContentView.getParent();
